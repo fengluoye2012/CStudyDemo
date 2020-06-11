489c50d11a019e5237a273cff624dcef49e8d9b5
fengluoye2012 <1522988552@qq.com>
2020-06-12 00:15:26 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/CStudyDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CStudyDemo.dir/main.c.o"
+  "CStudyDemo"
+  "CStudyDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CStudyDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
